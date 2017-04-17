f699d5a4bb2562bb2b35dbe3a0e03298944c14ca
Sam Wang <sam.zz.wang@gmail.com>
2017-04-17 15:42:47 -0700

    Recreate database, complete phase I

--- first_routes/db/migrate/20170417213747_remove_columns.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumns < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :name
-    remove_column :users, :email
-  end
-end
--- first_routes/db/migrate/20170417214030_add_column_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :username, :string
-    change_column_null :users, :username, false
-
-    # add_index :users, :username, unique: true
-  end
-end
